718bb9cf4c1db49b3dcc7911c6062cdf01082873
raghad fahad <ralmughidi@gmail.com>
2023-01-05 19:39:14 +0300

    upload img file &logo img ,header page extends

---
+++ resources/views/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="navbar container">
+        <a href="#" class="logo"> <img src="{{asset("img/logo.png")}}" alt="logo"></a>
+        <ul>
+            <li>
+                <a class="nav-item" href="#">الرئيسية</a>
+            </li>
+            <li>
+                <a class="nav-item" href="#">من نحن</a>
+            </li>
+            <li>
+                <a class="nav-item" href="#">خدماتنا</a>
+            </li>
+            <li> <a class="nav-item" href="#">توصل معنا</a></li>
+        </ul>
+        <div class="acount-btn">
+            <a class="btn" href=""> الدخول</a>
+            <a class="btn register" href="">انشاء حساب</a>
+        </div>
+    </div>
+</header>
