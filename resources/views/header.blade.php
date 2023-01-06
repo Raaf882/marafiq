ba93d0ace53abe01fb245c0a7a57e48097335a40
raghad fahad <ralmughidi@gmail.com>
2023-01-06 14:25:45 +0300

    fix log in btn

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -15,16 +15,29 @@
                 <a class="nav-item" href="#">خدماتنا</a>
             </li>
             <li> <a class="nav-item" href="footer">توصل معنا</a></li>
-        </ul>  
-        <div class="acount-btn">
+        </ul> 
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
-            <a class="bnt" href="{{ route('login') }}">Login</a>
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline btn">Home</a>
+            @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline btn">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline btn">Register</a>
+                @endif
+            @endif
+        </div>
+    @endif 
+        {{-- <div class="acount-btn">
+            @auth
+            <a class="btn" href="{{ route('login') }}">Login</a>
 
             @if (Route::has('register'))
-                <a class="bnt" href="{{ route('register') }}">Register</a>
+                <a class="btn" href="{{ route('register') }}">Register</a>
             @endif
         @endif
-        </div>
+        </div> --}}
     </div>
     
 </header>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Auth::routes();
 
