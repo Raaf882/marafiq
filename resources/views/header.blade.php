c389ebd3aa5519f48df000e997ec0e2cc20a37d6
raghad fahad <ralmughidi@gmail.com>
2023-01-07 19:50:27 +0300

    fix logout btn

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -14,20 +14,40 @@
             </li>
         
         </ul> 
-
-        @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline btn">Home</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline btn">الدخول</a>
-        
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline btn">تسجيل</a>
+        <ul class="navbar-nav ms-auto">
+            <!-- Authentication Links -->
+            @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="btn nav-link" href="{{ route('login') }}">{{ __('دخول') }}</a>
+                    </li>
                 @endif
-            @endif
-        </div>
-        @endif 
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="btn nav-link" href="{{ route('register') }}">{{ __('تسجيل') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class=" " style="display: flex; gapgit:1rem;">
+                    <a id="" class=" btn nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="" aria-labelledby="">
+                        <a class="btn" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('خروج') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="btn d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
         {{-- <div class="acount-btn">
             @auth
             <a class="btn" href="{{ route('login') }}">Login</a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Laravel') }}
+           
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -39,7 +39,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    {{-- <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -72,7 +72,7 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
+                    </ul> --}}
                 </div>
             </div>
         </nav>
