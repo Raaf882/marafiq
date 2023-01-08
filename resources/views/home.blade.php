df340152bf880989444aeaf68a58dd03e8b56c10
raghad fahad <ralmughidi@gmail.com>
2023-01-08 21:58:03 +0300

    add animation home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <title>مرافق</title>
 </head>
@@ -21,12 +22,12 @@
         <section id="home" style="background-color: #FAFBFA;">
             <div class=" home container">
 
-                <div class="home-content">
+                <div class="home-content" data-aos="fade-down" data-aos-duration="2000">
                     <h1 class="heading">مرافق للتأجير </h1>
                     <p class="title"><b>مساحات </b>استثنائية-لفعاليات حصرية </p>
                     <a class="btn hover submit" href="{{url ('/show_building')}}"> احجز الان</a>
                 </div>
-                <div class="home-img">
+                <div class="home-img" data-aos="fade-up" data-aos-duration="2000">
                     <img src="img/home-img.png" alt="home image">
                 </div>
             </div>
@@ -37,14 +38,14 @@
         <section id="about">
             <div class="aboutUs container">
 
-                <div class="aboutUs-card">
+                <div class="aboutUs-card" data-aos="fade-left" data-aos-duration="2000">
                     <h1>من نحن</h1>
                     <p>نقدم العديد من المرافق المتنوعة والحديثة المتاحة للحجز والاستئجار اصنعوا مساحاتكم هنا حيث نضمن
                         راحتكم مع العديد من الخدمات </p>
 
                     <a class="btn hover submit" href="{{url ('/show_building')}}"> احجز الان</a>
                 </div>
-                <div class="video-card card">
+                <div class="video-card card" data-aos="fade-up-right" data-aos-duration="2000">
                     <img src="/img/logo.png" alt="">
                 </div>
             </div>
@@ -52,27 +53,30 @@
         <!-- services -->
         <section id="service">
             <div class="services container">
-                <h1>نقدم لكم</h1>
+                <h1 data-aos="zoom-in-up">نقدم لكم</h1>
                 <div class="services-cards">
-                    <div class="service-card card">
+                    <div class="service-card card" data-aos="fade-up" data-aos-duration="2000">
                         <i class="ri-lightbulb-flash-line"></i>
                         <p>مرافق عالمية وملاعب</p>
                     </div>
-                    <div class="service-card card">
+                    <div class="service-card card"  data-aos="fade-up" data-aos-duration="2000">
                         <i class="ri-lightbulb-flash-line"></i>
                         <p>مرافق عالمية وملاعب</p>
                     </div>
-                    <div class="service-card card">
+                    <div class="service-card card"  data-aos="fade-up" data-aos-duration="2000">
                         <i class="i-s ri-lightbulb-flash-line"></i>
                         <p><b>مرافق عالمية وملاعب</b> </p>
                     </div>
                 </div>
-                <p class="title">مرافق عالمية لأفكار استثنائية</p>
+                <p class="title"data-aos="fade-up"data-aos-duration="3000">مرافق عالمية لأفكار استثنائية</p>
             
                 <a class="btn submit" href="{{url('/show_building')}}"> احجز الان</a>
             </div>
         </section>
-
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+            AOS.init();
+          </script>
     </body>
 @extends('footer')
    
